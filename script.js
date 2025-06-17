7a9998db556a4b7d8057ea5e08eedda21bb8f509
Santiago Villa Panesso <128158314+S12-Chawis@users.noreply.github.com>
2025-06-17 07:57:35 -0500

    Taller js - Santiago Villa

---
+++ script.js
@@ -0,0 +1,7 @@
+let names, age, city;
+
+names = prompt('What is your name?')
+age = prompt('How old are you?')
+city = prompt('Where are you from? (city)')
+
+console.log(`Hello ${names}, thank you for telling us that you are ${age} years old and your current city is ${city}`)
